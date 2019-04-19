ec63f2c063ef8c9f801367a4728b4aabbde86e1b
Jazel Canseco <jazel.canseco@gmail.com>
2019-04-18 23:51:25 -0400

    git: Add configs

---
+++ git/scripts/git_reset_hard.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Helper script for resetting the git working tree to HEAD.
+
+read -n1 -p "git: Resetting the working tree to HEAD. Continue? [Y/n]: " opt
+printf "\n"
+if [[ $opt == "Y" ]]; then
+    git add -A && git reset --hard
+fi
